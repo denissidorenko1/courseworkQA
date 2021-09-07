902c1b6058c4aeefff61d740a92a4f490158fce5
Denis <fpv.airplane.barnaul@gmail.com>
2021-09-07 18:05:13 +0300

    hooking up tests to CI

--- djangoProject/scripts/ui_tests.py
+++ djangoProject/scripts/ui_tests.py
@@ -2,11 +2,21 @@ from selenium import webdriver
 import unittest
 from selenium.webdriver.common.keys import Keys
 import time
+from webdriver_manager.chrome import ChromeDriverManager
 
 time.sleep(5)
 class Test2(unittest.TestCase):
     def setUp(self):
-        self.driver =webdriver.Chrome("C:\chromedriver\chromedriver.exe")
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('w3c', False)
+
+        options.add_experimental_option("useAutomationExtension", False)
+        options.add_argument('--disable-blink-features=AutomationControlled')
+        options.add_experimental_option('excludeSwitches', ['enable-automation'])
+
+        options.add_argument("--lang=en-GB")
+        # self.driver =webdriver.Chrome("C:\chromedriver\chromedriver.exe") I don't work on Windows anymore
+        self.driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
         self.driver.get("http://127.0.0.1:8000")
 
     def testSimplePath(self):
